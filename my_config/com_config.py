2188b147f96bde0603ff0fc0a4c2977c799b9b78
Lei.Wang99 <821609514@qq.com>
2023-08-26 14:23:51 +0800

    feat: 优化配置项
    1. 取消统一管理task开关
    2. 删除获取task配置路径
    3. 新增打印作者信息，因为每个task都需要

--- my_config/com_config.py
+++ my_config/com_config.py
@@ -1,11 +1,6 @@
 import os
 
 '''
-*********** 签到开关设置 ***********
-'''
-BAIDUTIEBA_ON = True
-
-'''
 *********** 消息推送配置 ***********
 如没有配置则不推送消息,为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.
 如果你实在不会,就直接用明文吧（O.o）
@@ -31,9 +26,14 @@ PRIVATE_AES_KEY = os.environ.get("PRIVATE_AES_KEY")
 LOG_LEVEL = 'INFO'
 
 
-def get_tieba_cf_path():
-    return os.path.normpath(
-        os.path.join(
-            os.path.dirname(os.path.abspath(__file__))
-            , 'tieba_config')
-    )
+def get_author_info(project):
+    '''
+    打印作者信息
+    '''
+    return f'''
+        **************************************
+            欢迎使用: {project}
+            作者GitHub：https://github.com/3 9 7 1 7 9 4 5 9
+            vx：L 3 9 7 1 7 9 4 5 9 加好友注明来意
+        **************************************
+        '''
